3638465a3ff1250424fb318b93f7723febcab2d9
asuka1211 <maks1211@bk.ru>
2021-04-27 14:47:19 +0300

    add Splash Screen

---
+++ app/src/main/java/com/serma/dionysus/ui/splash/SplashScreenViewModel.kt
@@ -0,0 +1,32 @@
+package com.serma.dionysus.ui.splash
+
+import androidx.lifecycle.viewModelScope
+import com.serma.dionysus.auth.manager.SessionManager
+import com.serma.dionysus.common.mvi.simple.SimpleMviViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashScreenViewModel @Inject constructor(private val sessionManager: SessionManager) :
+    SimpleMviViewModel<SplashViewStateState>() {
+
+    init {
+        checkSession()
+    }
+
+    override fun createInitialState(): SplashViewStateState = SplashViewStateState()
+
+    private fun checkSession() {
+        viewModelScope.launch {
+            val authState = if (sessionManager.isAuth()) {
+                AuthStateSplash.AUTH
+            } else {
+                AuthStateSplash.NOT_AUTH
+            }
+            delay(500L) //TODO: Добавить анимацию
+            _uiState.emit(currentState.copy(isAuth = authState))
+        }
+    }
+}
